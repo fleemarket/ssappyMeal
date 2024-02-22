1d1bcddf1636ecd860889e25561f3cd69409ba84
fleemarket <questional777@gmail.com>
2024-02-22 14:01:46 +0900

    Feat: 헤더 로고 클릭시 에듀 싸피 홈페이지로 이동

--- src/header/MainHeader.jsx
+++ src/header/MainHeader.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 
 export default function MainHeader({ day }) {
-  
   const day_indicator_KR = [
     "일요일",
     "월요일",
@@ -14,19 +13,25 @@ export default function MainHeader({ day }) {
 
   const today_day = day_indicator_KR[day];
 
-
-
   return (
     <div className="flex flex-row border-b border-gray-400 w-full h-[10%] p-2">
       <div className="flex flex-row justify-center w-1/5">
-        <img src="/logossafy.png" alt="empty image" className="h-full" />
+        <a href="https://edu.ssafy.com/">
+          <img src="/logossafy.png" alt="empty image" className="h-full" />
+        </a>
       </div>
       <div className="w-4/5 flex items-center p-1 gap-2">
-        <div className="w-2/5 flex text-xl m-3 items-center">오늘은... 
-        {day === 5 ? <div className=" flex text-red-500 font-semibold m-1 items-center">금요일! 집갈때 까지 화이팅!</div> : <div className=" flex  m-1 items-center">{today_day}</div>}
+        <div className="w-2/5 flex text-xl m-3 items-center">
+          오늘은...
+          {day === 5 ? (
+            <div className=" flex text-red-500 font-semibold m-1 items-center">
+              금요일! 집갈때 까지 화이팅!
+            </div>
+          ) : (
+            <div className=" flex  m-1 items-center">{today_day}</div>
+          )}
         </div>
       </div>
     </div>
   );
 }
-
